e5cf77159e2bfb0cc7e4fcccf48c45819ae605a7
parikhpreksha <88531258+parikhpreksha@users.noreply.github.com>
2021-11-15 10:58:28 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+package com.mycompany.brick;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+    
+}
